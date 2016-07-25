5097c59ed77ae09d4250aeda8646083c31ed26f8
Michael Knoch <info@michaelknoch.de>
2016-07-25 17:42:47 +0200

    implement component

---
+++ ng2-components.ts
@@ -0,0 +1,92 @@
+import {Component, OnChanges, ElementRef, Input} from '@angular/core';
+declare var jQuery: any;
+declare var cytoscape: any;
+
+@Component({
+    selector: 'ng2-cytoscape',
+    template: '<div id="cy"></div>',
+    styles: [`#cy {
+        height: 100%;
+        width: 100%;
+        position: absolute;
+        left: 0;
+        top: 0;
+    }`]
+})
+
+
+export class NgCytoscape implements OnChanges {
+
+    @Input() public elements: any;
+    @Input() public style: any;
+    @Input() public layout: any;
+    @Input() public zoom: any;
+
+    public constructor(private el: ElementRef) {
+
+        this.layout = this.layout || {
+                name: 'grid',
+                directed: true,
+                padding: 20
+            };
+
+        this.zoom = this.zoom || {
+                min: -1,
+                max: 2
+            };
+
+        this.style = this.style || cytoscape.stylesheet()
+                .selector('node')
+                .css({
+                    'content': 'data(name)',
+                    'shape': 'rectangle',
+                    'text-valign': 'center',
+                    'background-color': 'data(faveColor)',
+                    'width': '200px',
+                    'height': '100px',
+                    'color': 'black'
+                })
+                .selector(':selected')
+                .css({
+                    'border-width': 3,
+                    'border-color': '#333'
+                })
+                .selector('edge')
+                .css({
+                    'label': 'data(label)',
+                    'color': 'black',
+                    'curve-style': 'bezier',
+                    'opacity': 0.666,
+                    'width': 'mapData(strength, 70, 100, 2, 6)',
+                    'target-arrow-shape': 'triangle',
+                    'line-color': 'data(faveColor)',
+                    'source-arrow-color': 'data(faveColor)',
+                    'target-arrow-color': 'data(faveColor)'
+                })
+                .selector('edge.questionable')
+                .css({
+                    'line-style': 'dotted',
+                    'target-arrow-shape': 'diamond'
+                })
+                .selector('.faded')
+                .css({
+                    'opacity': 0.25,
+                    'text-opacity': 0
+                });
+    }
+
+    public ngOnChanges(): any {
+        this.render();
+    }
+
+    public render() {
+        jQuery(this.el.nativeElement).cytoscape({
+            layout: this.layout,
+            minZoom: this.zoom.min,
+            maxZoom: this.zoom.max,
+            style: this.style,
+            elements: this.elements,
+        });
+    }
+
+}
